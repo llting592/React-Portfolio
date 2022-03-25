7c3b1b9dd3199a498ac7973509aaeda800024af2
llting592 <llting592@gmail.com>
2022-03-25 10:04:49 -0400

    set up files for deployment

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import About from "./components/About/About";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+
+function App(){
+  return (
+    <main>
+      <About/>
+    </main>
+  )
+} 
+
+export default App;
---
+++ portfolio/src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import "./About.css";
+
+const About = () => {
+    return(
+        <div className = "About">
+            <div className="About-left">
+                <h1 classname= "title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                    Hi, I'm Lloyd!
+                </h1>
+                <p className= "About-me">
+                    Welcome to my portfolio page. I am a web developer based out of NYC, with a J.D. degree and a B.A. in Psychology. 
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
